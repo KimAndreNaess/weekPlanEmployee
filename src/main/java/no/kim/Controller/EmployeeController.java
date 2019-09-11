ee6b6d3e8ef6420cb48ee86be31ad837ebfabb4b
Kim <kimandreness@gmail.com>
2019-09-11 09:20:04 +0200

    Added a runable main method

--- src/main/java/no/kim/Controller/EmployeeController.java
+++ src/main/java/no/kim/Controller/EmployeeController.java
@@ -2,13 +2,20 @@ package no.kim.Controller;
 
 import no.kim.Entity.Employee;
 import no.kim.Service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 @RestController
+@RequestMapping("/employees")
 public class EmployeeController {
+
+    @Autowired
     private EmployeeService employeeService;
 
+    @RequestMapping(method = RequestMethod.GET)
     public Collection<Employee> getAllEmployees(){
         return employeeService.getAllEmployees();
     }
--- src/main/java/no/kim/Service/EmployeeService.java
+++ src/main/java/no/kim/Service/EmployeeService.java
@@ -2,12 +2,13 @@ package no.kim.Service;
 
 import no.kim.Dao.EmployeeDao;
 import no.kim.Entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 @Service
 public class EmployeeService {
-
+    @Autowired
     private EmployeeDao employeeDao;
 
     public Collection<Employee> getAllEmployees(){
